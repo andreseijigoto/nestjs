250acaee50e3048232c3b2162005a8f2ec0442d5
Andre Seiji Goto <andreseijigoto@gmail.com>
2025-08-21 10:46:10 -0300

    feat: clean project, config prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+const bootstrap = async (): Promise<void> => {
+  try {
+    const app = await NestFactory.create(AppModule)
+    await app.listen(process.env.PORT ?? 3000)
+  } catch (error) {
+    console.error(error)
+  }
 }
-bootstrap();
+
+void bootstrap()
